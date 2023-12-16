f1dbf47cceff976801fef71449db955e261ee733
ParvDev <parvdev0109@gmail.com>
2023-12-17 01:27:20 +0530

    redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-
-import './App.css'
+import {useDispatch} from "react-redux"
+import { useState, useEffect } from 'react'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import { Header, Footer} from "./components/index"
+import "./App.css"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>{throw error})
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-700">
+      <div className="w-full block">
+        <Header />
+        <main>
+          Todo: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
